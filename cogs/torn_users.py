c73b03ddbbf1bf7b4c816f28152794d4aa74ea59
Brian Mosier <bmosier@gmail.com>
2023-05-16 15:47:50 -0400

    untrack and tracking methods with error handling

--- cogs/torn_users.py
+++ cogs/torn_users.py
@@ -1,4 +1,5 @@
 from discord.ext import tasks, commands
+from re import search
 
 
 class TornUsers(commands.Cog):
@@ -17,19 +18,53 @@ class TornUsers(commands.Cog):
     @commands.command(name="track")
     async def add_track_user(self, ctx, user_id:str = ''):
         # TODO: fix default always tracking rip torn
-        r = self.torn_client.getUserStatus(user_id=user_id)
         subscriber = ctx.author
+        if self.is_valid_user_id(user_id):
+            try:
+                r = self.torn_client.getUserStatus(user_id=user_id)
 
-        # TODO: this should be a list of player ids and statuses
-        self.tracked_users[subscriber] = { str(r['player_id']): '' }
-        await subscriber.send(f"Now tracking {r['name']}")
+                # check if subscriber exists, init new dict
+                player_id = str(r['player_id'])
+                if not self.tracked_users.get(subscriber):
+                    self.tracked_users[subscriber] = {}
+                self.tracked_users[subscriber][player_id] = ''
+
+                await subscriber.send(f"Now tracking {r['name']}")
+            except Exception as e:
+                await subscriber.send(str(e))
+        else:
+            await subscriber.send(f"{user_id} is not a valid user id")
+
+
+    @commands.command(name="untrack")
+    async def remove_track_user(self, ctx, user_id:str = ''):
+        subscriber = ctx.author
+        if not self.is_valid_user_id(user_id):
+            await subscriber.send(f"{user_id} is not a valid user id.")
+        else:
+            try:
+                self.tracked_users[subscriber].pop(user_id)
+            except KeyError as e:
+                print(f"Error removing user id: {user_id}")
+            finally:
+                await subscriber.send(f"No longer tracking {user_id}")
+
+
+    @commands.command(name="tracking")
+    async def get_tracked_users(self, ctx):
+        subscriber = ctx.author
+        users = self.tracked_users.get(subscriber)
+        msg = "You are not tracking any users"
+        if users:
+            msg = f"Tracking {' '.join(users)}"
+
+        await subscriber.send(msg)
 
 
     @tasks.loop(seconds=60)
     async def check_users(self):
         if len(self.tracked_users.keys()) > 0:
             for subscriber in self.tracked_users.keys():
-                # TODO: this should iterate all the tracked statuses for the subscriber
                 for user_id, last_status in self.tracked_users[subscriber].items():
                     r = self.torn_client.getUserStatus(user_id=user_id)
                     status = r['status']['description']
@@ -39,3 +74,14 @@ class TornUsers(commands.Cog):
                         # dont spam hospital timers
                         if not ("hospital" in last_status.lower() and "hospital" in status.lower()):
                             await subscriber.send(f"{r['name']} is now {status}")
+
+
+    # Helper to check for empty, too short, too long
+    # and invalid user Ids
+    def is_valid_user_id(self, user_id:str):
+        user_id = user_id.strip()
+        if not user_id or \
+            len(user_id) != 7 or \
+            search("[a-zA-Z]*\s", user_id):
+            return False
+        return True
