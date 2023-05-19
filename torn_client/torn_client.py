4e74ed4ccfe2a9d18fa9aa8caf979dc454cafe79
Brian Mosier <bmosier@gmail.com>
2023-05-19 12:40:50 -0400

    update client with retries

--- torn_client/torn_client.py
+++ torn_client/torn_client.py
@@ -1,4 +1,5 @@
 import requests
+import time
 from .torn_statics.torn_endpoints import TornEndpoints as TE
 from .torn_statics.torn_selections import TornSelctions as TS
 
@@ -9,13 +10,13 @@ class TornClient:
     def __init__(self, client_config):
         self.LISTING_FEE = client_config['ITEM_MARKET_LISTING_FEE']
         self.API_KEY = client_config['API_KEY']
+        self.MAX_RETRIES = client_config['MAX_RETRIES']
 
     # Returns a dict of all torn items
     # https://api.torn.com/torn/?selections=items&key={self.API_KEY}"
     def getTornItems(self):
         url = self.generateURL(TE.TORN, TS.Torn.ITEMS)
-        r = requests.get(url)
-        return r.json()
+        return self.get_request(url)
 
 
     # Returns a dict of all Torn stocks available
@@ -23,8 +24,7 @@ class TornClient:
     # https://api.torn.com/torn/25?selections=stocks&key=API_KEY
     def getTornStock(self, stock_id: str="", key:str =None):
         url = self.generateURL(TE.TORN, TS.Torn.STOCKS, stock_id, key)
-        r = requests.get(url)
-        return r.json()['stocks']
+        return self.get_request(url)['stocks']
 
 
     # Returns a specific users' stocks based on who owns the API key
@@ -33,16 +33,14 @@ class TornClient:
     # https://api.torn.com/user/?selections=stocks&key={self.API_KEY}
     def getUserStock(self, user_id: str='', key: str=None):
         url = self.generateURL(TE.USER, TS.User.STOCKS, user_id, key)
-        r = requests.get(url)
-        return r.json()['stocks']
+        return self.get_request(url)['stocks']
 
     # Returns the status for a specific user
     # If no ID/API key provided it will use the owner user's ID and API key
     #   https://api.torn.com/user/{user_id}?selections=newevents&key={self.API_KEY}
     def getUserStatus(self, user_id: str='', key: str=None):
         url = self.generateURL(TE.USER, TS.User.BASIC, user_id, key)
-        r = requests.get(url)
-        return r.json()
+        return self.get_request(url)
 
 
     # Returns new events for a specific user
@@ -50,8 +48,7 @@ class TornClient:
     #   https://api.torn.com/user/{user_id}?selections=newevents&key={self.API_KEY}
     def getUserNewEvents(self, user_id: str='', key: str=None):
         url = self.generateURL(TE.USER, TS.User.NEWEVENTS, user_id, key)
-        r = requests.get(url)
-        return r.json()['events']
+        return self.get_request(url)['events']
 
 
     # returns item market listings for an ID
@@ -63,8 +60,7 @@ class TornClient:
     #   }, ... ]
     def getMarketItem(self, item_id: str='', key: str=None):
         url = self.generateURL(TE.MARKET, TS.Market.ITEMMARKET, item_id, key)
-        r = requests.get(url)
-        return r.json()['itemmarket']
+        return self.get_request(url)['itemmarket']
 
 
     # Returns a url string similar to:
@@ -75,6 +71,21 @@ class TornClient:
             .replace('SELECTIONS', selections) \
             .replace('ID', id) \
             .replace('KEY', key or self.API_KEY)
+
+
+    def get_request(self, url:str):
+        retry_count = 0
+        while retry_count < self.MAX_RETRIES:
+            retry_count+=1
+            try:
+                json_info = requests.get(url).json()
+                if not json_info.get('error'):
+                    return json_info
+            except requests.exceptions.RequestException as e:
+                print(f"Try: {retry_count} - {e}")
+                if retry_count >= self.MAX_RETRIES:
+                    raise e
+                time.sleep(5)
 
 
 def main():
